a3e7d203b541e83cba744cd439e51029d8e63ed1
TuxtonTech <klxrider11@gmail.com>
2025-09-08 16:32:38 -0400

    asd

--- app/ecosystem.config.js
+++ app/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'generalLister-app',
-    script: './build',
+    script: './start.js',
     instances: 1,
     exec_mode: 'cluster',
     env_file: '.env',
