a6bdb24204c92c43d5700bf1fd9343fb84b53d63
TuxtonTech <klxrider11@gmail.com>
2025-09-08 14:30:16 -0400

    asd

--- app/src/routes/pages/[page]/+page.ts
+++ app/src/routes/pages/[page]/+page.ts
@@ -1,12 +1,6 @@
-import type { PageLoad } from './$types';
+import { env } from '$env/dynamic/private';
 
-export const load = (async ({ params, fetch, url }) => {
-    console.log(await params);
-    const searchTerm = url.searchParams.get('s');
-    // const res = await fetch('https://www.google.com/')
-    // const text = await res.text()
-    return {
-        title: params.page,
-        searchTerm,
-    };
-}) satisfies PageLoad;
+export async function load() {
+    console.log('Environment variables loaded:', Object.keys(env));
+    return {};
+}
--- app/svelte.config.js
+++ app/svelte.config.js
@@ -3,17 +3,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	// preprocess: vitePreprocess(),
-	kit: { adapter: adapter({
-		bodySizeLimit: 50 * 1024 * 1024
-	}) },
-	env: {
-      publicPrefix: 'PUBLIC_'
-    }
+	preprocess: vitePreprocess(), // You should enable this
+	kit: { 
+		adapter: adapter({
+			// For adapter-node, this should be 'bodySize', not 'bodySizeLimit'
+			bodySize: 50 * 1024 * 1024 // 50MB
+		}),
+		// Move env config inside kit
+		env: {
+			publicPrefix: 'PUBLIC_'
+		}
+	}
 };
-
-
 
 export default config;
